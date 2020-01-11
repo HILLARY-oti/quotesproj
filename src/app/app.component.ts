1c1850c1403f4a0ff83441f135684121c6bfa4f3
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-12 00:06:34 +0300

    add parmenently removed some contents and added to quote.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,5 @@ import { Quote } from './quote'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote = [
-    {id:1, name:'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.'},
-    {id:2, name:'Raise your word,not your Voice.It is rain that grows flowers,not thunder.'},
-  ];
+  
 }
