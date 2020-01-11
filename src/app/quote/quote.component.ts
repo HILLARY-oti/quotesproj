bcd2648d9ba1a68b4301eeeb3eff27109182414c
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-12 00:08:24 +0300

    add arrays

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import{ Quote }from'../quote'
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote = [
+    {id:1, name:'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.'},
+    {id:2, name:'Raise your word,not your Voice.It is rain that grows flowers,not thunder.'},
+  ];
 
   constructor() { }
 
