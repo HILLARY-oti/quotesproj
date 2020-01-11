bc09dab7d063da9f92d4b40f9cca401ff59f6ee3
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-12 00:34:17 +0300

    add description

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,10 +8,11 @@ import { Quote }from'../quote';
 })
 export class QuoteComponent implements OnInit {
 
-  quotes:Quote = [
-    {id:1, name:'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.'},
-    {id:2, name:'Raise your word,not your Voice.It is rain that grows flowers,not thunder.'}
+  quotes: Quote[] = [
+    {id:1, name:'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.',description:'Author:Martin Luther'},
+    {id:2, name:'Raise your word,not your Voice.It is rain that grows flowers,not thunder.',description:'Author:Jhon Stone'}
   ];
+  
   constructor() { }
 
   ngOnInit() {
