342004375805bec9c8f9856b5e34f1d359dff823
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-11 23:34:17 +0300

    add update the component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Quotes';
+  quote = 'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.'
 }
