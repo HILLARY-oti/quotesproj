020defca8169a675fff872b7335b5960404e0224
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-12 22:48:37 +0300

    add voteup and down logic and button

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Quote }from '../quote';
 
+
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
@@ -14,6 +15,18 @@ export class QuoteDetailComponent implements OnInit {
   quoteComplete(complete:boolean){
     this.isComplete.emit(complete);
   }
+
+
+  downvote;
+  upvote;
+  voteDown(){
+    this.downvote=this.downvote+1;
+  }
+
+  
+   voteUp(){
+     this.upvote=this.upvote+1;
+   }
 
   constructor() { }
 
