55d42a732384781380fb5c55b5e793d0aed36664
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-12 14:59:57 +0300

    add changed an error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Quote } from './quote'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote = [
-    {id:1, name:'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.'},
-    {id:2, name:'Raise your word,not your Voice.It is rain that grows flowers,not thunder.'}
-  ];
+  quotes: Quote[] = [
+    new Quote(1,'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.','"Martin Luther"'),
+     new Quote(2,'Raise your word,not your Voice.It is rain that grows flowers,not thunder.','"Jhon Stone"')
+   ];
 }
