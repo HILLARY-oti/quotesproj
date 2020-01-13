74651b6ee0317646a6518ddb465d883814896faa
HILLARY-oti <hillaryotieno646@gmail.com>
2020-01-13 12:08:57 +0300

    add new date description

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Quote } from './quote'
 })
 export class AppComponent {
   quotes: Quote[] = [
-    new Quote(1,'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.','"Martin Luther"'),
-     new Quote(2,'Raise your word,not your Voice.It is rain that grows flowers,not thunder.','"Jhon Stone"')
+    new Quote(1,'God and Nature first made us what we are, and then out of our own created genius we make ourselves what we want to be. Follow always that great law. Let the sky and God be our limit and Eternity our measurement.','"Martin Luther"', new Date(2019, 3, 14)),
+     new Quote(2,'Raise your word,not your Voice.It is rain that grows flowers,not thunder.','"Jhon Stone"', new Date(2020, 1, 7))
    ];
 }
